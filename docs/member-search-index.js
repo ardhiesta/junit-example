bc44dae2dc42512962ee5367453d1136ec3d9c4f
ardhiesta <ardhi.mail@gmail.com>
2022-12-05 10:31:40 +0700

    first commit

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Kalkulator","l":"Kalkulator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Kalkulator","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Kalkulator","l":"tambah(double, double)","u":"tambah(double,double)"}];updateSearchResults();
